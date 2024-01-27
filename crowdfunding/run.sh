6d3b11a50c5ab60cac635e5e8d556f91da43d3bb
Suan Choi <siasiarhdwn@gmail.com>
2024-01-27 12:25:24 +0800

    fix: remove secret key

--- crowdfunding/run.sh
+++ crowdfunding/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python manage.py migrate
-python manage.py createsuperuser --no-input
+python3 manage.py migrate
+python3 manage.py createsuperuser --no-input
 gunicorn --bind :8000 --workers 1 crowdfunding.wsgi
